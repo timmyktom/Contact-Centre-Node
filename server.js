4d3dbed063d636cca6f2f8b4a6077db1dbc24e8e
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-06-15 11:22:37 +0100

    basic version
    0.1
    
    uses assignment callback, single workflow

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const accountSid = process.env.TWILIO_ACME_ACCOUNT_SID;
 const authToken = process.env.TWILIO_ACME_AUTH_TOKEN;
 const workspaceSid = process.env.TWILIO_ACME_WORKSPACE_SID;
 const client = require('twilio')(accountSid, authToken);
-
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const workflow_sid = process.env.TWILIO_ACME_SUPPORT_WORKFLOW_SID;
+const caller_id =process.env.TWILIO_ACME_CALLERID;
+const wrap_up =process.env.TWILIO_ACME_WRAP_UP_ACTIVTY;
 
 // Register '.html' extension with The Mustache Express
 app.engine('html', mustacheExpress());
@@ -25,7 +28,54 @@ app.get('/', function (req, res) {
  	
 });
 
+app.get('/incoming_call', function(req, res) {
 
+const response = new VoiceResponse();
+
+const gather = response.gather({
+  input: 'speech dtmf',
+  timeout: 3,
+  numDigits: 1,
+  action:'/enqueue_call'
+});
+
+gather.say('please select from the following options');
+gather.say('for sales press one, for support press two');
+gather.say('for billing press three, for marketing press 4');
+
+res.send(response.toString());
+
+});
+
+app.get('/enqueue_call', function(req, res){
+
+const response = new VoiceResponse();
+    var Digits = req.query.Digits;
+
+    var product = {
+      1: 'sales',
+      2: 'support',
+      3: 'marketing'
+    }
+    
+    const enqueue = response.enqueue({workflowSid:workflow_sid });  
+    enqueue.task({}, JSON.stringify({selected_product: product[Digits]}));
+
+    res.type('text/xml');
+
+    res.send(response.toString());
+
+});
+
+app.get('/assignment_callback', function(req, res){
+
+dequeue = {"instruction": "dequeue", "from": caller_id, "post_work_activity_sid": wrap_up}
+
+res.type('application/json');
+
+    res.send(dequeue);
+
+});
 app.get('/agent_list', function (req, res) {
      client.taskrouter.v1
   .workspaces(workspaceSid)
@@ -33,11 +83,11 @@ app.get('/agent_list', function (req, res) {
   .list( { TargetWorkersExpression: 'worker.channel.chat.configured_capacity > 0' })
 .then((workers) => {
 	//console.log(workers);
-	var voice_workers = workers;
+	var voice_workers =  workers;
   //workers.forEach((worker) => console.log(worker.friendlyName));
- // res.set({ 'content-type': 'application/json; charset=utf-8' })
+ //res.set({ 'content-type': 'application/json; charset=utf-8' })
  console.log(voice_workers);
-  res.render('agent_list.html', {'voice_workers': voice_workers});
+  res.render('agent_list.html', {'voice_workers': voice_workers.toString()});
 	 });
  	
 });
