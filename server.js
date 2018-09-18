79459a315ede957876dac06d76d3f173524f81f7
Thomas Curtis <thomas.e.curtis@gmail.com>
2018-09-18 16:03:04 +0100

    fixed token generation (again)
    reworked worker_token function

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const util = taskrouter.util;
 const TaskRouterCapability = taskrouter.TaskRouterCapability;
 const Policy = TaskRouterCapability.Policy;
 
-
 const app = express();
 
 const accountSid = process.env.TWILIO_ACME_ACCOUNT_SID;
@@ -30,14 +29,14 @@ const version = 'v1';
 const ClientCapability = require('twilio').jwt.ClientCapability;
 
 function buildWorkspacePolicy(options, context) {
-  const taskrouter = Twilio.jwt.taskrouter;
+  const taskrouter = twilio.jwt.taskrouter;
   const TaskRouterCapability = taskrouter.TaskRouterCapability;
   const Policy = TaskRouterCapability.Policy;
   options = options || {};
   var version = 'v1';
   var resources = options.resources || [];
   const TASKROUTER_BASE_URL = 'https://' + 'taskrouter.twilio.com';
-  var urlComponents = [TASKROUTER_BASE_URL, version, 'Workspaces', context.TWILIO_WORKSPACE_SID]
+  var urlComponents = [TASKROUTER_BASE_URL, version, 'Workspaces', workspaceSid]
   return new Policy({
     url: urlComponents.concat(resources).join('/'),
     method: options.method || 'GET',
@@ -46,11 +45,6 @@ function buildWorkspacePolicy(options, context) {
 }
 
 app.use(express.static(__dirname + '/public'));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -70,7 +64,7 @@ app.get('/', function (req, res) {
 
 });
 
-app.get('/incoming_call', function (req, res) {
+app.post('/incoming_call', function (req, res) {
 
   const response = new VoiceResponse();
 
@@ -89,7 +83,7 @@ app.get('/incoming_call', function (req, res) {
 
 });
 
-app.get('/enqueue_call', function (req, res) {
+app.post('/enqueue_call', function (req, res) {
 
   const response = new VoiceResponse();
   var Digits = req.query.Digits;
@@ -109,20 +103,18 @@ app.get('/enqueue_call', function (req, res) {
 
 });
 
-app.get('/assignment_callback', function (req, res) {
+app.post('/assignment_callback', function (req, res) {
 
   dequeue = { "instruction": "dequeue", "from": caller_id, "post_work_activity_sid": wrap_up }
-
   res.type('application/json');
 
-  res.send(dequeue);
+  res.json(dequeue);
 
 });
 
 app.get('/agent_list', function (req, res) {
   res.render('agent_list.html');
 });
-
 
 app.post('/agent_list', function (req, res) {
   client.taskrouter.v1
@@ -143,8 +135,6 @@ app.get('/agents', function (req, res) {
   res.render('agent_desktop.html');
 });
 
-
-
 app.post('/activities', function (req, res) {
   var list = [];
 
@@ -159,97 +149,72 @@ app.post('/activities', function (req, res) {
       res.send(activities);
 
     });
-
-
 })
 
-app.post('/worker_token', function(req, res){
+app.use('/worker_token', function (req, res) {
 
   let jwt = require('jsonwebtoken');
-  const response = new Twilio.Response();
-  
-  response.appendHeader('Access-Control-Allow-Origin', '*');
-  response.appendHeader('Access-Control-Allow-Methods', 'POST');
-  response.appendHeader('Content-Type', 'application/json');
-  response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-  
-  const worker = event.workerSid;
-  const taskrouter = Twilio.jwt.taskrouter;
-  const util = Twilio.jwt.taskrouter.util;
+  //Set access control headers to avoid CORBs issues
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const workerSid = req.body.WorkerSid;
+  const taskrouter = twilio.jwt.taskrouter;
+  const util = twilio.jwt.taskrouter.util;
   const TaskRouterCapability = taskrouter.TaskRouterCapability;
   const capability = new TaskRouterCapability({
-    accountSid: context.ACCOUNT_SID,
-    authToken: context.AUTH_TOKEN,
-    workspaceSid: context.TWILIO_WORKSPACE_SID,
-    channelId: worker,
-    ttl: 2880});
+    accountSid: accountSid,
+    authToken: authToken,
+    workspaceSid: workspaceSid,
+    channelId: workerSid,
+    ttl: 2880
+  });
   // Event Bridge Policies
-  var eventBridgePolicies = util.defaultEventBridgePolicies(context.ACCOUNT_SID, worker);
+  var eventBridgePolicies = util.defaultEventBridgePolicies(accountSid, workerSid);
+
   var workspacePolicies = [
     // Workspace fetch Policy
-    buildWorkspacePolicy({}, context),
+    buildWorkspacePolicy(),
+    // Workspace subresources fetch Policy
+    buildWorkspacePolicy({ resources: ['**'] }),
     // Workspace Activities Update Policy
-    buildWorkspacePolicy({ resources: ['Activities'], method: 'POST' }, context),
-    buildWorkspacePolicy({ resources: ['Activities'], method: 'GET' }, context),
-    //
+    buildWorkspacePolicy({ resources: ['Activities'], method: 'POST' }),
+    buildWorkspacePolicy({ resources: ['Activities'], method: 'GET' }),
+    // Workspace Activities Task Policy
+
+    buildWorkspacePolicy({ resources: ['Tasks', '**'], method: 'POST' }),
+    buildWorkspacePolicy({ resources: ['Tasks', '**'], method: 'GET' }),
     
-    buildWorkspacePolicy({ resources: ['Tasks', '**'], method: 'POST'  }, context),
-    buildWorkspacePolicy({ resources: ['Tasks', '**'], method: 'GET' }, context),
-    // Workspace Activities Worker Reserations Policy
-    buildWorkspacePolicy({ resources: ['Workers', worker, 'Reservations', '**'], method: 'POST' }, context),
-    buildWorkspacePolicy({ resources: ['Workers', worker, 'Reservations', '**'], method: 'GET' }, context),
-    //
-    buildWorkspacePolicy({ resources: ['Workers', worker, 'Channels', '**'], method: 'POST' }, context),
-    buildWorkspacePolicy({ resources: ['Workers', worker, 'Channels', '**'], method: 'GET' }, context),
-    // Workspace Activities Worker  Policy
-    buildWorkspacePolicy({ resources: ['Workers', worker], method: 'GET' }, context),
-    buildWorkspacePolicy({ resources: ['Workers', worker], method: 'POST' }, context),
+    // Workspace Worker Reservation Policy
+    buildWorkspacePolicy({ resources: ['Workers', workerSid, 'Reservations', '**'], method: 'POST' }),
+    buildWorkspacePolicy({ resources: ['Workers', workerSid, 'Reservations', '**'], method: 'GET' }),
+
+    // Workspace Worker Channel Policy
+
+    buildWorkspacePolicy({ resources: ['Workers', workerSid, 'Channels', '**'], method: 'POST' }),
+    buildWorkspacePolicy({ resources: ['Workers', workerSid, 'Channels', '**'], method: 'GET' }),
+
+    // Workspace Worker  Policy
+
+    buildWorkspacePolicy({ resources: ['Workers', workerSid], method: 'GET' }),
+    buildWorkspacePolicy({ resources: ['Workers', workerSid], method: 'POST' }),
+
   ];
+
   eventBridgePolicies.concat(workspacePolicies).forEach(function (policy) {
     capability.addPolicy(policy);
   });
-  response.setBody({token: capability.toJwt()});
-  console.log(response.body);
-  callback(null, response);
+
+  var token = capability.toJwt();
+
+  res.json(token);
+
 })
-
-// app.post('/worker_token', function (req, res) {
-
-//   const workerSid = req.body.WorkerSid;
-
-//   const capability = new TaskRouterCapability({
-//     accountSid: accountSid,
-//     authToken: authToken,
-//     workspaceSid: workspaceSid,
-//     channelId: workerSid
-//   });
-
-//   var eventBridgePolicies = util.defaultEventBridgePolicies(accountSid, workerSid);
-
-//   var workspacePolicies = [
-//     // Workspace fetch Policy
-//     buildWorkspacePolicy(),
-//     // Workspace subresources fetch Policy
-//     buildWorkspacePolicy({ resources: ['**'] }),
-//     // Workspace Activities Update Policy
-//     buildWorkspacePolicy({ resources: ['Activities'], method: 'POST' }),
-//     // Workspace Activities Worker Reserations Policy
-//     buildWorkspacePolicy({ resources: ['Workers', workerSid, 'Reservations', '**'], method: 'POST' }),
-//   ];
-
-//   eventBridgePolicies.concat(workspacePolicies).forEach(function (policy) {
-//     capability.addPolicy(policy);
-//   });
-
-//   var token = capability.toJwt();
-
-//   res.send(token);
-// });
 
 
 app.post('/client_token', function (req, res) {
-  //let response = new twilio.Response();
-
 
   const identity = req.body.WorkerSid;
 
